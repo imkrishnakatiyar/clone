5cbcdc76e9c05eac4d8c5e9dc46e768f2551c4c8
imkrishnakatiyar <90976299+imkrishnakatiyar@users.noreply.github.com>
2025-03-12 15:07:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,31 @@
-export default function App() {
-  return <h1>Hello world</h1>
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { Home, Search, Library, Player } from "./pages";
+import { Sidebar, Header } from "./components";
+import YouTube from "react-youtube";
+
+function App() {
+  const [videoId, setVideoId] = useState("dQw4w9WgXcQ"); // Default video
+
+  return (
+    <Router>
+      <div className="flex h-screen bg-black text-white">
+        <Sidebar />
+        <div className="flex-1 flex flex-col">
+          <Header />
+          <main className="flex-1 overflow-y-auto p-4">
+            <Routes>
+              <Route path="/" element={<Home setVideoId={setVideoId} />} />
+              <Route path="/search" element={<Search setVideoId={setVideoId} />} />
+              <Route path="/library" element={<Library />} />
+            </Routes>
+          </main>
+          <Player videoId={videoId} />
+        </div>
+      </div>
+    </Router>
+  );
 }
+
+export default App;
+
